0520327edee8da3ea039cc42b171f24dbe926170
Christopher Shaw <chris@chris-shaw.com>
2018-02-12 22:25:15 +0000

    Added Options
    Default: Show only online
    -a: Show All

--- online.sh
+++ online.sh
@@ -2,14 +2,27 @@
 # Christopher Shaw (https://www.chris-shaw.com)
 # Feb 2018
 
+view='onlineOnly'
+
+while getopts 'abf:v' flag; do
+         case "${flag}" in
+                a) view='' ;;
+         esac
+done
+
 echo "Locating Online Devices";
 for ip in $(seq 1 254);
 do
+
         ping -c 1 192.168.0.$ip> /dev/null;
+
         if [ $? = 0 ]
         then
                 echo "192.168.0.$ip - online";
         else
-                echo "192.168.0.$ip - offline";
+                if [ "$view" != "onlineOnly" ];
+                then
+                        echo "192.168.0.$ip - offline";
+                fi
         fi
-done                                                                                                              
+done
